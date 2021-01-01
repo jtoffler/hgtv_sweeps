2267bf17796a4e8b56bdbcefbd2f279abedd7354
Jake Toffler <jake.toffler@gmail.com>
2021-01-01 12:15:27 -0500

    fixed typo

--- lib/python/enter_sweeps.py
+++ lib/python/enter_sweeps.py
@@ -115,7 +115,7 @@ if __name__ == '__main__':
                 try:
                     wait.until(EC.element_to_be_clickable((By.XPATH, submit_xpath)))
                     fails = 0
-                    retry_click(driver, submit_path, fails)
+                    retry_click(driver, submit_xpath, fails)
 
                     fails = 0
                     retry_click(driver, submit_xpath, fails)
