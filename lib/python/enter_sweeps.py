f3941d33a1af6153c2acd6b5046a55ef479d954c
Jake Toffler <jake.toffler@gmail.com>
2021-01-03 16:35:51 -0500

    removed ElementNotInteractableException

--- lib/python/enter_sweeps.py
+++ lib/python/enter_sweeps.py
@@ -29,7 +29,7 @@ def retry_keys(driver, email_xpath, email, fails):
             wait = WebDriverWait(driver, 5)
             wait.until(EC.presence_of_element_located((By.XPATH, email_xpath)))
             driver.find_element(By.XPATH, email_xpath).send_keys(email)
-        except (ElementNotInteractableException, NoSuchElementException, StaleElementReferenceException):
+        except (NoSuchElementException, StaleElementReferenceException):
             fails += 1
             retry_keys(driver, email_xpath, email, fails)
     else:
@@ -45,7 +45,7 @@ def retry_click(driver, xpath, fails):
             wait = WebDriverWait(driver, 5)
             wait.until(EC.element_to_be_clickable((By.XPATH, advance_xpath)))
             driver.find_element(By.XPATH, xpath).click()
-        except (ElementNotInteractableException, NoSuchElementException, StaleElementReferenceException):
+        except (NoSuchElementException, StaleElementReferenceException):
             fails += 1
             retry_click(driver, xpath, fails)
     else:
